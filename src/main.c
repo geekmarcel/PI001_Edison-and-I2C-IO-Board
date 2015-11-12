85a95fa7e73c628202b36949c6963bdd2b08cb19
Marcel@Home <geekmarcel@outlook.com>
2015-11-12 22:15:06 +0100

    Setup test LED blink

--- src/main.c
+++ src/main.c
@@ -1,16 +1,34 @@
+
+
+#include "mraa.h"
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-int main() {
+int main()
+{
   /* Setup your example here, code that should run once
    */
 
+	mraa_platform_t platform = mraa_get_platform_type();
+	mraa_gpio_context d_pin = NULL;
 
-  /* Code in this loop will run repeatedly
-   */
-  for (;;) {
+	d_pin = mraa_gpio_init(13);
 
-  }
+	// set the pin as output
+	if (mraa_gpio_dir(d_pin, MRAA_GPIO_OUT) != MRAA_SUCCESS) {
+		fprintf(stderr, "Can't set digital pin as output, exiting");
+		return MRAA_ERROR_UNSPECIFIED;
+	};
 
-  return 0;
+	// loop forever toggling the on board LED every second
+	for (;;) {
+		mraa_gpio_write(d_pin, 0);
+		sleep(3);
+		mraa_gpio_write(d_pin, 1);
+		sleep(1);
+	}
+
+	return MRAA_SUCCESS;
 }
